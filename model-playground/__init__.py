1c9af7be7d1941c1562335952eda73d8ed2ff22e
jzhang1198 <jzhang1198@gmail.com>
2023-08-31 12:08:07 -0700

    Minor changes to __init__.py

---
+++ model-playground/__init__.py
@@ -0,0 +1,9 @@
+"""
+model-playground: a python library for visualizing how changes to free
+parameters affects the behavior of user-defined mathematical models.
+"""
+
+from . import models
+from . import plotter
+
+__version__ = "0.1.1"
