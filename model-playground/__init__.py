99a72b45b86bd09ccfd8373634e1bb7da253c99c
jzhang1198 <jzhang1198@gmail.com>
2023-09-05 09:29:13 -0700

    Bulk update

--- model-playground/__init__.py
+++ model-playground/__init__.py
@@ -6,4 +6,4 @@ parameters affects the behavior of user-defined mathematical models.
 from . import models
 from . import plotter
 
-__version__ = "0.1.1"
+__version__ = "1.1.1"
--- model-playground/setup.py
+++ model-playground/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model-playground',
-    version='0.1.1',
+    version='1.1.1',
     packages=find_packages(),
     author= 'Jonathan Zhang',
     author_email="jon.zhang@ucsf.edu",
