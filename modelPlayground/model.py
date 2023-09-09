61762f5b5c7bf7f3362e17d4c07d931ad7c29813
jzhang1198 <jzhang1198@gmail.com>
2023-09-09 14:42:32 -0700

    Added method for generating model slider data

--- modelPlayground/model.py
+++ modelPlayground/model.py
@@ -1,15 +1,38 @@
 import inspect
-from typing import Callable
+import numpy as np
+from numbers import Number
+from typing import Callable, Union
 
 class IndependentVariableCollection:
-    def __init__(self, names: list, value_arrays: list, units:list=[]):
+    def __init__(self, names: Union[list, np.ndarray], value_arrays: Union[list, np.ndarray], units: Union[list, np.ndarray]=[]):
+        """
+        A utility class for organizing data associated with model independent variables.
+        """
 
-        assert len(names) == len(value_arrays), 'ERROR: The length of names and values lists must be identical.'
-
+        IndependentVariableCollection._parse_inputs(names, value_arrays, units)
         self.names = names
         self.value_arrays = value_arrays
         self.units = units if len(units) > 0 else [None] * len(names)
     
+    @staticmethod
+    def _parse_inputs(names, value_arrays, units):
+        """ 
+        Private static method for ensuring input parameters satisfy assumptions of the program.
+        """
+
+        inputs = (names, value_arrays, units)
+
+        # ensure consistent length and datatype of input arrays
+        input_length_set = set([len(input) for input in inputs])
+        input_length_set.discard(0)
+        assert len(input_length_set) == 1, 'IndependentVariableCollection Error: The length of input lists must be identical.'
+        assert set([type(input) for input in inputs]).issubset(set([np.ndarray, list])), 'IndependentVariableCollection Error: Inputs must be lists or np.ndarrays.'
+
+        # ensure value_arrays contains elements of the correct datatype
+        for value_array in value_arrays:
+            assert type(value_array) == list or type(value_array) == np.ndarray, "IndependentVariableCollection Error: Elements within value_arrays must be lists or np.ndarrays."
+            assert set([isinstance(value, Number) for value in value_array]) == set([True]), "IndependentVariableCollection Error: Numeric data is required for value_arrays."
+
     def get_names(self):
         return self.names
     
@@ -20,15 +43,42 @@ class IndependentVariableCollection:
         return self.units
 
 class ParameterColllection:
-    def __init__(self, names: list, initial_values: list, lower_bounds:list=[], upper_bounds:list=[], units:list=[]):
+    def __init__(
+            self, 
+            names: 
+            Union[list, np.ndarray], 
+            initial_values: Union[list, np.ndarray], 
+            lower_bounds: Union[list, np.ndarray], 
+            upper_bounds: Union[list, np.ndarray], 
+            units: Union[list, np.ndarray]=[]):
+        """
+        A utility class for organizing data associated with model parameters.
+        """
 
-        assert len(names) == len(initial_values), 'ERROR: The length of names, values, upper_bounds, and lower_bounds lists must be identical.' 
-
+        ParameterColllection._parse_inputs(names, initial_values, lower_bounds, upper_bounds, units)
         self.names = names
         self.initial_values = initial_values
         self.lower_bounds = lower_bounds if len(lower_bounds) > 0 else [None] * len(names)
-        self.upper_bounds = upper_bounds if len(upper_bounds) > 0 else [None] * len(upper_bounds)
+        self.upper_bounds = upper_bounds if len(upper_bounds) > 0 else [None] * len(names)
         self.units = units if len(units) > 0 else [None] * len(names)
+
+    @staticmethod
+    def _parse_inputs(names, initial_values, lower_bounds, upper_bounds, units):
+        """ 
+        Private static method for ensuring input parameters satisfy assumptions of the program.
+        """
+
+        inputs = (names, initial_values, lower_bounds, upper_bounds, units)
+
+        # ensure consistent length and datatype of input arrays
+        input_length_set = set([len(input) for input in inputs])
+        input_length_set.discard(0)
+        assert len(input_length_set) == 1, 'ParameterColllection Error: The length of input lists must be identical.'
+        assert set([type(input) for input in inputs]).issubset(set([np.ndarray, list])), 'ParameterColllection Error: Inputs must be lists or np.ndarrays.'
+
+        assert set([isinstance(value, Number) for value in initial_values]) == set([True]), "ParameterColllection Error: Numeric data is required for initial_values."
+        assert set([isinstance(value, Number) for value in lower_bounds]) == set([True]), "ParameterColllection Error: Numeric data is required for lower_bounds."
+        assert set([isinstance(value, Number) for value in upper_bounds]) == set([True]), "ParameterColllection Error: Numeric data is required for upper_bounds."
 
     def get_names(self):
         return self.names
@@ -55,17 +105,14 @@ class Model:
             model_units:str = None
             ):
         
-        # parse function signature of the model to ensure consistency with collections
-        signature = inspect.signature(model)
-        arguments = list(signature.parameters.keys())
-        assert len(arguments) == len(independent_variable_collection.get_names()) + len(parameter_collection.get_names())
-        assert set(arguments) == set(independent_variable_collection.get_names()).union(set(parameter_collection.get_names()))
+        Model._parse_inputs(model, independent_variable_collection, parameter_collection)
         
         self.model = model
         self.model_name = model_name
         self.model_units = model_units
         self.independent_variable_collection = independent_variable_collection
         self.parameter_collection = parameter_collection
+        self.slider_data = self._generate_slider_data()
 
         # private dictionaries to map parameter names to values or value arrays
         self._independent_variable_dictionary = dict([(argname, value_range) for argname, value_range in zip(self.independent_variable_collection.get_names(), self.independent_variable_collection.get_value_arrays())])
@@ -73,7 +120,45 @@ class Model:
         self._argument_dictionary = dict(
             **self._independent_variable_dictionary, 
             **self._parameter_dictionary)
+        
+    def _parse_inputs(model, independent_variable_collection, parameter_collection):
 
+        # parse function signature of the model to ensure consistency with collections
+        signature = inspect.signature(model)
+        arguments = list(signature.parameters.keys())
+        assert len(arguments) == len(independent_variable_collection.get_names()) + len(parameter_collection.get_names()), 'Model Error: '
+        assert set(arguments) == set(independent_variable_collection.get_names()).union(set(parameter_collection.get_names())), 'Model Error: '
+
+        assert type(independent_variable_collection) == IndependentVariableCollection, 'Model Error: independent_variable_collection must be an instance of the IndependentVariableCollection class.'
+        assert type(parameter_collection) == ParameterColllection, 'Model Error: parameter_collection must be an instance of the ParameterColllection class.'
+
+    def _generate_slider_data(self):
+
+        NO_STEPS = 100 # default number of steps
+        SCALE = 'linear' # default slider scale
+
+        slider_datas = []
+        for name, initial_value, lower_bound, upper_bound, unit in zip(
+            self.parameter_collection.get_names(),
+            self.parameter_collection.get_initial_values(),
+            self.parameter_collection.get_lower_bounds(),
+            self.parameter_collection.get_upper_bounds(),
+            self.parameter_collection.get_units()
+        ):
+            stepsize = (upper_bound - lower_bound) / (NO_STEPS - 1)
+            slider_datas.append(
+                {
+                    'name': name,
+                    'initial_value': initial_value,
+                    'min': lower_bound,
+                    'max': upper_bound,
+                    'scale': SCALE,
+                    'stepsize': stepsize
+                }
+            )
+
+        return slider_datas
+        
     def evaluate(self):
         """ 
         Evaluates the model with set independent variables and parameters.
