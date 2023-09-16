98e5b3c5c2021cfaa72e83fd17ef29d4704a8629
jzhang1198 <jzhang1198@gmail.com>
2023-09-16 13:53:01 -0700

    Update to plot.js

--- static/scripts/plot.js
+++ static/scripts/plot.js
@@ -1,28 +1,22 @@
+async function fetchPlotData() {
+    try {
+        const response = await fetch('/serve_plot_data');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const plotData = await response.json();
+        console.log(plotData); // Use the data here
+        return plotData;
+    } catch (error) {
+        console.error('Error fetching slider data:', error);
+        return null;
+    }
+}
+
 async function initPlot() {
 
-    const plotData = await fetch('/send_initial_data')
-        .then(response => { return response.json() });
+    const plotData = await fetchPlotData();
 
-    plotData['type'] = 'scatter';
-    plotData['mode'] = 'line';
+    Plotly.newPlot('plot-container', plotData['traces'], plotData['layout']);
 
-    const layout = {
-        title: 'Test',
-        xaxis: {
-            title: 'X-axis',
-            aspectratio: 1, // Set the aspect ratio for the x-axis
-        },
-        yaxis: {
-            title: 'Y-axis',
-            aspectratio: 1, // Set the aspect ratio for the y-axis
-        },
-    };
-    
-
-    Plotly.newPlot('plot-container', [plotData], layout);
-
-    }
-
-document.addEventListener("DOMContentLoaded", function() {
-    initPlot();
-});
+}
