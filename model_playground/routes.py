6836029974dd11292f2acf327cbd326ea40a4b1c
jzhang1198 <jzhang1198@gmail.com>
2023-09-16 14:23:31 -0700

    Bulk update to accomodate importation of route-handling functions in run_app.py

--- model_playground/routes.py
+++ model_playground/routes.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import render_template, request, jsonify
 from .model import Model
 import sys
 
-app = Flask(__name__)
-
-# utility function for reading in a model
 def read_model(model_file: str):
     """
     Reads a variables within an input model file into memory.
@@ -22,73 +19,61 @@ def read_model(model_file: str):
             model = value
     return model
 
-# route for serving static content
-@app.route('/')
-def serve_static_content(model_file):
-    global MODEL
+def exec_serve_static_content(model_file: str):
 
     # read the model into memory
-    MODEL = read_model(model_file)
+    model = read_model(model_file)
 
-    if not MODEL:
+    if not model:
         print('ERROR: Model was not successfully read into memory. Ensure that you have constructed a Model object in your model file.')
         sys.exit()
 
-    return render_template('index.html', model_name=MODEL.name)
+    return model, render_template('index.html', model_name=model.name)
 
-# route for handling slider data requests
-@app.route('/serve_slider_data', methods=['GET'])
-def serve_slider_data():
-    return jsonify(MODEL.slider_data)
+def exec_serve_slider_data(model):
+    return jsonify(model.slider_data)
 
-# route for handling plot data requests
-@app.route('/serve_plot_data', methods=['GET'])
-def serve_plot_data():
+def exec_serve_plot_data(model: Model, plot_data: dict):
 
-    global PLOT_DATA
-
-    x = MODEL.independent_variable_collection.get_value_arrays()
+    x = model.independent_variable_collection.get_value_arrays()
     x = x[0] if len(x) == 1 else x
-    xlabel = MODEL.independent_variable_collection.get_names()
+    xlabel = model.independent_variable_collection.get_names()
     xlabel = xlabel[0] if len(x) == 1 else xlabel
 
     # generate the layout for independent variables
-    assert len(MODEL.independent_variable_collection.get_value_arrays()) < 3, 'LocalServerError: Only can plot functions with a maximum of two independent variables!'
-    layout = {'title': MODEL.name}
-    for index, name in enumerate(MODEL.independent_variable_collection.get_names()):
+    assert len(model.independent_variable_collection.get_value_arrays()) < 3, 'LocalServerError: Only can plot functions with a maximum of two independent variables!'
+    layout = {'title': model.name}
+    for index, name in enumerate(model.independent_variable_collection.get_names()):
         axis = 'xaxis' if index == 0 else 'yaxis'
         layout[axis] = {'title': name, 'aspectratio': 1}
 
     # generate data for traces and layout for model prediction(s)
     traces = []
-    for index, trace in enumerate(MODEL.evaluate()):
+    for index, trace in enumerate(model.evaluate()):
 
         trace_data = {'type': 'scatter', 'mode': 'lines','x': x, 'y': trace}
-        name = MODEL.prediction_names[index] if len(MODEL.prediction_names) < index else None
+        name = model.prediction_names[index] if len(model.prediction_names) < index else None
 
         if name:
             trace_data['name'] = name
         
         traces.append(trace_data)
 
-    PLOT_DATA['traces'] = traces
+    plot_data['traces'] = traces
     axis = 'yaxis' if axis == 'xaxis' else 'zaxis'
-    layout['axis'] = {'title': MODEL.name, 'aspectratio': 1}
-    PLOT_DATA['layout'] = layout
+    layout['axis'] = {'title': model.name, 'aspectratio': 1}
+    plot_data['layout'] = layout
 
-    return jsonify(PLOT_DATA)
+    return plot_data, jsonify(plot_data)
 
-# route for handling update requests
-@app.route('/update_plot_data', methods=['POST'])
-def update_plot_data():
+def exec_update_plot_data(model: Model, plot_data: dict):
 
     data = request.json
     param_name = str(data['paramName'])
     param_value = float(data['paramValue'])
-    MODEL.update_parameter(param_name, param_value)
+    model.update_parameter(param_name, param_value)
 
-    global PLOT_DATA
-    for index, trace in enumerate(MODEL.evaluate()):
-        PLOT_DATA['traces'][index]['y'] = trace
+    for index, trace in enumerate(model.evaluate()):
+        plot_data['traces'][index]['y'] = trace
 
-    return jsonify(PLOT_DATA)
+    return model, plot_data, jsonify(plot_data)
--- run_app.py
+++ run_app.py
@@ -3,100 +3,38 @@
 import os
 import sys
 import argparse
-from model_playground.model import Model
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
+from model_playground.routes import exec_serve_static_content, exec_serve_plot_data, exec_serve_slider_data, exec_update_plot_data
 
 app = Flask(__name__, static_url_path='/static', template_folder='./templates')
-PLOT_DATA = {}
-MODEL = None
-
-def read_model(model_file: str):
-    """
-    Reads a variables within an input model file into memory.
-    """
-
-    # execute the model file and get variables
-    model = None
-    variables = {}
-    with open(model_file, 'r') as file:
-        exec(file.read(), variables)
-
-    # identify variable containing the model
-    for value in variables.values():
-        if type(value) == Model:
-            model = value
-    return model
+MODEL_FILE, MODEL, PLOT_DATA = None, None, {}
 
 # route for serving static content
 @app.route('/')
 def serve_static_content():
     global MODEL
-
-    # read the model into memory
-    MODEL = read_model(model_file)
-
-    if not MODEL:
-        print('ERROR: Model was not successfully read into memory. Ensure that you have constructed a Model object in your model file.')
-        sys.exit()
-
-    return render_template('index.html', model_name=MODEL.name)
+    MODEL, response = exec_serve_static_content(MODEL_FILE) 
+    return response
 
 # route for handling slider data requests
 @app.route('/serve_slider_data', methods=['GET'])
 def serve_slider_data():
-    return jsonify(MODEL.slider_data)
+    response = exec_serve_slider_data(MODEL)
+    return response
 
 # route for handling plot data requests
 @app.route('/serve_plot_data', methods=['GET'])
 def serve_plot_data():
-
     global PLOT_DATA
-
-    x = MODEL.independent_variable_collection.get_value_arrays()
-    x = x[0] if len(x) == 1 else x
-    xlabel = MODEL.independent_variable_collection.get_names()
-    xlabel = xlabel[0] if len(x) == 1 else xlabel
-
-    # generate the layout for independent variables
-    assert len(MODEL.independent_variable_collection.get_value_arrays()) < 3, 'LocalServerError: Only can plot functions with a maximum of two independent variables!'
-    layout = {'title': MODEL.name}
-    for index, name in enumerate(MODEL.independent_variable_collection.get_names()):
-        axis = 'xaxis' if index == 0 else 'yaxis'
-        layout[axis] = {'title': name, 'aspectratio': 1}
-
-    # generate data for traces and layout for model prediction(s)
-    traces = []
-    for index, trace in enumerate(MODEL.evaluate()):
-
-        trace_data = {'type': 'scatter', 'mode': 'lines','x': x, 'y': trace}
-        name = MODEL.prediction_names[index] if len(MODEL.prediction_names) < index else None
-
-        if name:
-            trace_data['name'] = name
-        
-        traces.append(trace_data)
-
-    PLOT_DATA['traces'] = traces
-    axis = 'yaxis' if axis == 'xaxis' else 'zaxis'
-    layout['axis'] = {'title': MODEL.name, 'aspectratio': 1}
-    PLOT_DATA['layout'] = layout
-
-    return jsonify(PLOT_DATA)
+    PLOT_DATA, response = exec_serve_plot_data(MODEL, PLOT_DATA)
+    return response
 
 # route for handling update requests
 @app.route('/update_plot_data', methods=['POST'])
 def update_plot_data():
-
-    data = request.json
-    param_name = str(data['paramName'])
-    param_value = float(data['paramValue'])
-    MODEL.update_parameter(param_name, param_value)
-
-    global PLOT_DATA
-    for index, trace in enumerate(MODEL.evaluate()):
-        PLOT_DATA['traces'][index]['y'] = trace
-
-    return jsonify(PLOT_DATA)
+    global MODEL, PLOT_DATA 
+    MODEL, PLOT_DATA, response = exec_update_plot_data(MODEL, PLOT_DATA)
+    return response
 
 if __name__ == '__main__':
 
@@ -106,13 +44,14 @@ if __name__ == '__main__':
 
     # access arguments
     args = parser.parse_args()
-    model_file = args.model_file
+    MODEL_FILE = args.model_file
 
-    if not model_file: 
+    # ensure model file exists
+    if not MODEL_FILE: 
         print('ERROR: A model file is required to run the local server.')
         sys.exit()
 
-    elif not os.path.exists(model_file):
+    elif not os.path.exists(MODEL_FILE):
         print('ERROR: Model file not found.')
         sys.exit()
 
